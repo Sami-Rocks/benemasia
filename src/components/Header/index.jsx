ba56a2f8aea2d8cdb8ce8d1279aae2c25cf5e27a
Sami <sowusuaboagye@gmail.com>
2022-04-21 09:51:58 +0000

    responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,29 @@
 import './style.scss'
 import { headerData } from 'helpers/data'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header =()=>{
+
+    const location = useLocation()
+
     return(
         <header>
             <div className="container">
                 <div className='logo-nav' >
                     <div className="logo">
+                        <div className="menu">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                        </div>
                         <h2>Jasper</h2>
                     </div>
                     <nav>
                         <ul>
                             {headerData.map(data =>(
-                                <li key={data.id} className={`${data.id === 2 && 'active'}`} >
+                                <li key={data.id} className={`${data.link === location.pathname && 'active'}`} >
                                     <Link to={`${data.link || '/'}`} >{data.title} </Link>
-                                    { data.id === 2 && <span className='dot' ></span> } 
+                                    { data.link === location.pathname && <span className='dot' ></span> } 
                                 </li> 
                             ))}
                         </ul>
